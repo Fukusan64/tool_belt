f5ff18ad5baa29d176247856d5452cb1514d68a3
Fukusan64 <ikishikawa64@gmail.com>
2024-04-29 11:14:39 +0900

    pjs リファクタリング

--- pjs/main.mjs
+++ pjs/main.mjs
@@ -1,6 +1,7 @@
 #! /usr/bin/env zx
 import parser from './interpreter.mjs';
-import readline from 'node:readline'
+import readline from 'node:readline';
+
 const IS_ALL_READ_MODE = Object.keys(argv).some(v => v === 'a');
 
 const DELIMITER = argv.d;
@@ -12,7 +13,8 @@ const DEFAULT_TOPIC_REFERENCE = '@';
 const command = argv._[0] ?? (argv.a === true ? undefined : argv.a);
 
 if (command === undefined) {
-  const help = [
+  const help = 
+    [
       'pjs [options] command',
       'options',
       '    -a                  : load all mode',
@@ -31,7 +33,7 @@ if (command === undefined) {
 const processing = parser(
   command.toString() ?? '',
   DELIMITER ?? DEFAULT_DELIMITER,
-  TOPIC_REFERENCE ?? DEFAULT_TOPIC_REFERENCE
+  TOPIC_REFERENCE ?? DEFAULT_TOPIC_REFERENCE,
 );
 
 if (IS_ALL_READ_MODE) {
@@ -39,6 +41,6 @@ if (IS_ALL_READ_MODE) {
   for await (const chunk of process.stdin) input += chunk;
   processing(input);
 } else {
-  const rl = readline.createInterface({ input: process.stdin })
+  const rl = readline.createInterface({ input: process.stdin });
   for await (const line of rl) await processing(line);
 }
--- pjs/utils.mjs
+++ pjs/utils.mjs
@@ -1,10 +1,6 @@
+const ttyConsole = new console.Console(fs.createWriteStream('/dev/tty'));
 export const p = _ => {console.log(_);return _;};
-export const t = (() => {
-    const c = new console.Console(fs.createWriteStream('/dev/tty'));
-    return _ => {c.log(_);return _};
-})();
+export const t = _ => {ttyConsole.log(_);return _};
 export const e = _ => {console.error(_);return _;};
 
-export const vGlobal = (() => {
-    return {};
-})();
+export const vGlobal = {};
